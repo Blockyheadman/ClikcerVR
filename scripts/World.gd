8f4dc77cd555d93da6ac5416fb85ef37af0ca50f
Blockyheadman <80011716+Blockyheadman@users.noreply.github.com>
2022-12-10 11:48:19 -0600

    Delete scripts directory

--- scripts/World.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-func _ready():
-	pass
-
-func _on_ClikcerBtnPressed_body_entered(body):
-	if body.name == "ClikcerButton":
-		Stats.clikcs = Stats.clikcs + 1
-		$Clikcer/Screen1/ClikcsCounter3D/Viewport/ClikcsCounter.text = "Clikcs: " + str(Stats.clikcs)
